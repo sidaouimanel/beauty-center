a67685fa75245435703a14bbf012e37d60f0f37f
sidaoui <sidaouimanel@gmail.com>
2024-12-04 11:52:05 +0100

    Initial Commit

--- public/backend/server.js
+++ public/backend/server.js
@@ -1,4 +1,4 @@
-// src/server.js (ou index.js)
+// src/server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Importer le package CORS
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 const bookingRoutes = require('./routes/bookingRoutes'); // Importer les routes de réservation
 
 const app = express();
-const PORT = 5002;
+const PORT = 5002; // Choisir un port disponible
 
 // Middleware pour CORS
 app.use(cors()); // Autoriser toutes les origines
@@ -16,18 +16,23 @@ app.use(cors()); // Autoriser toutes les origines
 app.use(express.json());
 
 // Connexion à MongoDB
-mongoose.connect('mongodb://localhost:27017/beauty-base', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log('Failed to connect to MongoDB:', err));
+mongoose.connect("mongodb://127.0.0.1:27017/beauty-center")
+
+    .then(() => {
+        console.log('MongoDB connected...');
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB:', err.message);
+        console.error('Stack trace:', err.stack);
+        process.exit(1); // Arrêter l'application si la connexion échoue
+    });
 
 // Utilisation des routes
 app.use('/api', contactRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api', bookingRoutes); // Utiliser les routes de réservation
 
+// Démarrer le serveur
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
